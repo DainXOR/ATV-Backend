f9130dc6cdf5c4524fe95fc9f4026888f50946d2
Daniel León <65270757+DainXOR@users.noreply.github.com>
2025-10-10 13:19:51 -0500

    Diosito quiera esto no rompa nada

--- src/routes/info_routes.go
+++ src/routes/info_routes.go
@@ -12,63 +12,6 @@ import (
 )
 
 func InfoRoutes(router *gin.Engine) {
-	_ = gin.H{
-		"info": gin.H{
-			"root":          "/api/info/",
-			"ping":          "/api/info/ping",
-			"api version":   "/api/info/api-version",
-			"route version": "/api/info/route-version",
-		},
-
-		"test": gin.H{
-			"get":    "/api/test/get",
-			"post":   "/api/test/post",
-			"put":    "/api/test/put",
-			"patch":  "/api/test/patch",
-			"delete": "/api/test/del",
-		},
-
-		"v1": gin.H{
-			"student": gin.H{
-				"post":         "/api/v1/student/",
-				"get by id":    "/api/v1/student/:id",
-				"get all":      "/api/v1/student/all",
-				"put":          "/api/v1/student/:id",
-				"patch":        "/api/v1/student/:id",
-				"delete by id": "/api/v1/student/:id",
-				//"force delete by id": "/api/v1/student/permanent-delete/:id/:confirm",
-			},
-			"university": gin.H{
-				"post":      "/api/v1/university/",
-				"get by id": "/api/v1/university/:id",
-				"get all":   "/api/v1/university/all",
-			},
-			"speciality": gin.H{
-				"post":      "/api/v1/speciality/",
-				"get by id": "/api/v1/speciality/:id",
-				"get all":   "/api/v1/speciality/all",
-			},
-			"session type": gin.H{
-				"post":      "/api/v1/session-type/",
-				"get by id": "/api/v1/session-type/:id",
-				"get all":   "/api/v1/session-type/all",
-			},
-			"session": gin.H{
-				"post":      "/api/v1/session/",
-				"get by id": "/api/v1/session/:id",
-				"get all":   "/api/v1/session/all",
-			},
-			"companion": gin.H{
-				"post":         "/api/v1/companion/",
-				"get by id":    "/api/v1/companion/:id",
-				"get all":      "/api/v1/companion/all",
-				"put":          "/api/v1/companion/:id",
-				"patch":        "/api/v1/companion/:id",
-				"delete by id": "/api/v1/companion/:id",
-			},
-		},
-	}
-
 	availableRoutes := BuildRoutesInfo(router)
 	availableRoutes["info"] = gin.H{
 		"root":          "/api/info/",
